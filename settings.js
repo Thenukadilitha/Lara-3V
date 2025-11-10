14bf6aa3da50b1794da6a8ad7d9c98c3400477d0
Thenukadilitha <thenukadilitha123@gmail.com>
2025-11-10 18:29:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "ORY0HRKS#_mgJcUuNlv25WoZ7QmwRAw9xtiTrDHZD5aCTQ5q55p0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
